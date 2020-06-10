9363181b2e67c1c2acd7083e33a4802f75fadc07
Daniel Antonio Nolasco Alvarado <dnieln7@gmail.com>
2020-06-10 12:23:13 -0500

    demo 1 app movil

---
+++ Desarrollo/VAID/app/src/main/java/com/dnieln7/vaid/ui/MainActivity.java
@@ -0,0 +1,36 @@
+package com.dnieln7.vaid.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.dnieln7.vaid.R;
+import com.dnieln7.vaid.utils.Printer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.main_appbar).findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+    }
+
+    public void luces(View view) {
+        startActivity(new Intent(this, LucesActivity.class));
+    }
+
+    public void cerradura(View view) {
+        Printer.toast(this, "Cerraduras");
+    }
+
+    public void aire(View view) {
+        Printer.toast(this, "Aire");
+    }
+}
---
+++ Desarrollo/VAID/app/src/main/java/com/dnieln7/vaid/utils/Directorio.java
@@ -0,0 +1,16 @@
+package com.dnieln7.vaid.utils;
+import android.graphics.Color;
+
+public class Directorio {
+
+    public static final int PUERTO = 1444;
+
+    public static final String HB1 = "192.168.1.76";
+    public static final String HB2 = "192.168.1.76";
+
+    public static final int LUCES_ON = Color.rgb(255, 241, 118);
+    public static final int LUCES_OFF = Color.rgb(194, 194, 194);
+
+    private Directorio() {
+    }
+}

56c5d7dc229b09129127c017f462e9413c95ab5c
Daniel Antonio Nolasco Alvarado <dnieln7@gmail.com>
2020-06-15 10:36:15 -0500

    se agrego botones de navegacion en luces activity

--- Desarrollo/VAID/app/src/main/java/com/dnieln7/vaid/ui/LucesActivity.java
+++ Desarrollo/VAID/app/src/main/java/com/dnieln7/vaid/ui/LucesActivity.java
@@ -40,7 +40,9 @@ public class LucesActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.luces_appbar).findViewById(R.id.toolbar);
         toolbar.setTitle("Luces");
+        toolbar.setNavigationIcon(R.drawable.ic_less_arrow_left);
         setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
     }
 
     private static void cambiarEstado(CardView habitacion, boolean orden) {

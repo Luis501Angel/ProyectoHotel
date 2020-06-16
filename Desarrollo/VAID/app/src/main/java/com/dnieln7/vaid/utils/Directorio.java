ba328b51624e046ff4e23232cfc39e8745aa4592
Daniel Antonio Nolasco Alvarado <dnieln7@gmail.com>
2020-06-16 11:20:55 -0500

    arreglos

--- Desarrollo/VAID/app/src/main/java/com/dnieln7/vaid/utils/Directorio.java
+++ Desarrollo/VAID/app/src/main/java/com/dnieln7/vaid/utils/Directorio.java
@@ -5,7 +5,7 @@ public class Directorio {
 
     public static final int PUERTO = 1444;
 
-    public static final String HB1 = "192.168.1.68";
+    public static final String HB1 = "26.44.240.204";
     public static final String HB2 = "192.168.1.76";
 
     public static final int LUCES_ON = Color.rgb(255, 241, 118);
